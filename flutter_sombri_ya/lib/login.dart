b2723a8a89c6eb283c65decac03a0bb2a06d9b29
silvisangal <s.sandovalg@uniandes.edu.co>
2025-10-10 15:09:05 -0500

    Login and Sign in corrected

--- flutter_sombri_ya/lib/login.dart
+++ flutter_sombri_ya/lib/login.dart
@@ -4,7 +4,6 @@ import 'package:flutter_sombri_ya/forgot_password.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'home.dart';
-import "signin.dart";
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -18,15 +17,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final storage = const FlutterSecureStorage();
   final LocalAuthentication auth = LocalAuthentication();
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   Future<void> login(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
     final email = emailController.text;
     final password = passwordController.text;
 
-    final url = Uri.parse("https://sombri-ya-back-4def07fa1804.herokuapp.com/auth/login/password");
+    final url = Uri.parse(
+        "https://sombri-ya-back-4def07fa1804.herokuapp.com/auth/login/password");
     final response = await http.post(
       url,
       headers: {"Content-Type": "application/json"},
@@ -38,8 +45,6 @@ class _LoginPageState extends State<LoginPage> {
 
     if (response.statusCode == 200 || response.statusCode == 201) {
       final data = jsonDecode(response.body);
-      debugPrint("Respuesta loing: $data");
-
       final token = data["accessToken"] as String;
 
       Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
@@ -48,13 +53,23 @@ class _LoginPageState extends State<LoginPage> {
       await storage.write(key: "auth_token", value: token);
       await storage.write(key: "user_id", value: userId);
 
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Inicio de sesión exitoso"),
+          backgroundColor: Colors.green,
+        ),
+      );
+
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const HomePage()),
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Correo o contraseña incorrectos")),
+        const SnackBar(
+          content: Text("Correo o contraseña incorrectos"),
+          backgroundColor: Colors.red,
+        ),
       );
     }
   }
@@ -66,26 +81,24 @@ class _LoginPageState extends State<LoginPage> {
 
       if (googleUser == null) return;
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+      await googleUser.authentication;
       final idToken = googleAuth.idToken;
 
-
-      final url = Uri.parse("https://sombri-ya-back-4def07fa1804.herokuapp.com/auth/login/google");
+      final url = Uri.parse(
+          "https://sombri-ya-back-4def07fa1804.herokuapp.com/auth/login/google");
       final response = await http.post(
         url,
         headers: {"Content-Type": "application/json"},
         body: jsonEncode({"idToken": idToken}),
       );
 
-
       if (response.statusCode == 200 || response.statusCode == 201) {
         final data = jsonDecode(response.body);
         final token = data["accessToken"];
 
-
         await storage.write(key: "auth_token", value: token);
 
-        // Decodifica JWT para obtener user_id
         final Map<String, dynamic> decoded = JwtDecoder.decode(token);
         await storage.write(key: "user_id", value: decoded["sub"]);
 
@@ -95,21 +108,19 @@ class _LoginPageState extends State<LoginPage> {
         );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Error al iniciar con Google (${response.statusCode})")),
+          SnackBar(
+            content:
+            Text("Error al iniciar con Google (${response.statusCode})"),
+            backgroundColor: Colors.red,
+          ),
         );
       }
     } catch (e) {
-      print("Error en login con Google: $e");
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Error al iniciar con Google")),
       );
     }
   }
-
-
-
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +132,7 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               const SizedBox(height: 40),
               const Text(
-                "Sombri-ya",
+                "Sombri-Ya",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
@@ -129,6 +140,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               const SizedBox(height: 30),
+
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 20),
                 padding: const EdgeInsets.all(20),
@@ -137,164 +149,187 @@ class _LoginPageState extends State<LoginPage> {
                   borderRadius: BorderRadius.circular(20),
                   border: Border.all(color: Colors.grey, width: 2),
                 ),
-                child: Column(
-                  children: [
-                    const Text(
-                      "Iniciar sesión",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Icon(
-                      Icons.person,
-                      size: 50,
-                      color: Color(0xFF001242),
-                    ),
-                    const SizedBox(height: 15),
-                    TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: "Correo electrónico",
-                        hintText: "correo@ejemplo.com",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        filled: true,
-                        fillColor: Color(0xFFF2F2F2),
-                        contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Iniciar sesión",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
                         ),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Ingresa tu correo";
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 15),
-                    TextFormField(
-                      obscureText: true,
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        labelText: "Contraseña",
-                        hintText: "Contraseña",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        filled: true,
-                        fillColor: Color(0xFFF2F2F2),
-                        contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
+                      const SizedBox(height: 20),
+                      const Icon(
+                        Icons.person,
+                        size: 50,
+                        color: Color(0xFF001242),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Ingresa tu contraseña";
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.center,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => const ForgotPassword(),
-                            ),
-                          );
+                      const SizedBox(height: 15),
+
+
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Correo electrónico",
+                          hintText: "correo@ejemplo.com",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Ingresa tu correo";
+                          }
+                          final emailRegex =
+                          RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                          if (!emailRegex.hasMatch(value)) {
+                            return "Correo inválido";
+                          }
+                          return null;
                         },
-                        child: const Text(
-                          "¿Olvidaste tu contraseña?",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12,
-                            decoration: TextDecoration.underline,
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: "Contraseña",
+                          hintText: "Contraseña",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
                           ),
                         ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.center,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => const SigninPage(),
-                            ),
-                          );
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Ingresa tu contraseña";
+                          }
+                          return null;
                         },
-                        child: const Text(
-                          "¿No tienes una cuenta? Regístrate",
-                          style: TextStyle(color: Colors.grey, fontSize: 12, decoration: TextDecoration.underline,),
-                        ),
                       ),
-                    ),
 
-                    const SizedBox(height: 20),
+                      const SizedBox(height: 10),
 
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () => login(context),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF001242),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                        ),
-                        child: const Text(
-                          "Iniciar sesión",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFFFFFDFD),
+                      Align(
+                        alignment: Alignment.center,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => const ForgotPassword(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "¿Olvidaste tu contraseña?",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
 
-                    SizedBox(
-                      width: double.infinity,
-                      child: OutlinedButton.icon(
-                        onPressed: () => loginWithGoogle(context), // Login con Google
-                        icon: SizedBox(
-                          width: 10,
-                          height: 10, //
-                          child: Image.asset(
-                            'assets/images/google_logo2.png',
-                            fit: BoxFit.contain,
+                      const SizedBox(height: 10),
+
+                      Align(
+                        alignment: Alignment.center,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => const SigninPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "¿No tienes una cuenta? Regístrate",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
                         ),
-                        label: const Text(
-                          "Iniciar sesión con Google",
-                          style: TextStyle(fontSize: 16, color: Colors.black87),
-                        ),
-                        style: OutlinedButton.styleFrom(
-                          side: const BorderSide(color: Colors.grey),
-                          padding: const EdgeInsets.symmetric(vertical: 14),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () => login(context),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF001242),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding:
+                            const EdgeInsets.symmetric(vertical: 14),
+                          ),
+                          child: const Text(
+                            "Iniciar sesión",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFFFFFDFD),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+
+                      const SizedBox(height: 10),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton.icon(
+                          onPressed: () => loginWithGoogle(context),
+                          icon: SizedBox(
+                            width: 10,
+                            height: 10,
+                            child: Image.asset(
+                              'assets/images/google_logo2.png',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          label: const Text(
+                            "Iniciar sesión con Google",
+                            style: TextStyle(
+                                fontSize: 16, color: Colors.black87),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                            side: const BorderSide(color: Colors.grey),
+                            padding:
+                            const EdgeInsets.symmetric(vertical: 14),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
 
               const SizedBox(height: 40),
 
-              // Footer
               const Text(
                 "Sombri-Ya",
                 style: TextStyle(
